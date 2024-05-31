6cf612f4c609469edc1f6284b9d4d19590f3b3e2
ibrahimgcm <68804273+ibrahimgcm@users.noreply.github.com>
2024-05-31 12:27:47 +0300

    Add files via upload

---
+++ final.asm
@@ -0,0 +1,67 @@
+;Student Number             //2019556463
+;Student Name and Surname  //ibrahim göçmen
+;Github Username           //ibrahimgcm
+
+.model small
+.stack 100h
+
+.data
+    studentNumber DB '2', '0', '1', '9', '5', '5', '6', '4', '6', '3', '$'
+    morningMsg DB ' Morning', 0Dh, 0Ah, '$'
+    eveningMsg DB ' Evening', 0Dh, 0Ah, '$'
+
+.code
+    mov ax, @data
+    mov ds, ax
+    
+    
+    mov bx, 0100h
+    mov es, bx
+    mov di, 2000h
+    mov si, offset studentNumber
+    mov cx, 10  
+    rep movsb
+    
+    mov si, offset studentNumber
+    mov di, 0200h 
+    mov es, di 
+    mov di, 2000h 
+    mov cx, 10 
+    cld 
+    rep movsb 
+
+    mov ah, 09h
+    lea dx, [0200h:2000h]
+    int 21h
+    
+    
+    mov bx, 0200h
+    mov es, bx
+    mov si, 2006h
+    mov al, es:[si]
+    cmp al, '5'
+    je morning
+    cmp al, '6'
+    je evening
+    
+    mov ah, 09h
+    lea dx, morningMsg
+    int 21h
+    jmp end
+    
+morning:
+    mov ah, 09h
+    lea dx, morningMsg
+    int 21h
+    jmp end
+
+evening:    
+    mov ah, 09h
+    lea dx, eveningMsg
+    int 21h
+    
+end:
+    mov ah, 4Ch
+    int 21h
+
+end
